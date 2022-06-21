ef2909b5f91649ce28cd8956843dd9fe6438685e
Leonard Chi <leonardochi168@gmail.com>
2022-06-21 20:57:39 +0100

    Start > Blank Screen - Working On GameController Class

--- run.py
+++ run.py
@@ -12,3 +12,18 @@ class GameController(object):
     def setBackground(self):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
         self.background.fill(BLACK)
+
+    def startGame(self):
+        self.setBackground()
+
+    def update(self):
+        self.checkEvents()
+        self.render()
+
+    def checkEvents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+    def render(self):
+        pygame.display.update()
