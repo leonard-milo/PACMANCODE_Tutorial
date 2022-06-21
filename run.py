d93acce5029c233c41890d56e7082fe0e1385c9c
Leonard Chi <leonardochi168@gmail.com>
2022-06-21 20:50:25 +0100

    Start > Blank Screen - gamecontroller Class Starty

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 import pygame
 from pygame.locals import *
 from constants import *
+
+class GameController(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+        self.background = None
