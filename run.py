dc448d2702caa59dba3b0ca21d148bfee47eef1b
Leonard Chi <leonardochi168@gmail.com>
2022-06-21 21:12:42 +0100

    Start > Basic Movement - Make Pacman object

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from pacman import Pacman
 
 class GameController(object):
     def __init__(self):
@@ -17,9 +18,11 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
+        self.pacman = Pacman()
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
         self.checkEvents()
         self.render()
 
@@ -29,6 +32,8 @@ class GameController(object):
                 exit()
 
     def render(self):
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.render(self.screen)
         pygame.display.update()
 
 
