b25e51369cda87f6c4fc1a69428d91d9b9a11af5
Leonard Chi <leonardochi168@gmail.com>
2022-06-21 20:32:45 +0100

    Start > Vectors - Arithmetic Methods

--- vector.py
+++ vector.py
@@ -5,3 +5,26 @@ class Vector2(object):
         self.x = x
         self.y = y
         self.thresh = 0.000001
+
+# Arithmetic Methods
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return Vector2(-self.x, -self.y)
+
+    def __mul__(self, scalar):
+        return Vector2(self.x * scalar, self.y * scalar)
+
+    # Python 2 uses the __div__ method
+    def __div__(self, scalar):
+        if scalar != 0:
+            return Vector2(self.x / float(scalar), self.y / float(scalar))
+        return None
+
+    # Python 3 does away with the __div__ method in favor of the __truediv__ method
+    def __truediv__(self, scalar):
+        return self.__div__(scalar)
