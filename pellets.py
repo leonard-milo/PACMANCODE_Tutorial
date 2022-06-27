85169c4870bd2c7553909dad6e35065ca04ddebf
Leonard Chi <leonardochi168@gmail.com>
2022-06-27 15:44:37 +0100

    Level 2 > Pellets - Pelletgroup Class

--- pellets.py
+++ pellets.py
@@ -33,3 +33,37 @@ class PowerPellet(Pellet):
         if self.timer >= self.flashTime:
             self.visible = not self.visible
             self.timer = 0
+
+class PelletGroup(object):
+    def __init__(self, pelletfile):
+        self.pelletList = []
+        self.powerpellets = []
+        self.createPelletList(pelletfile)
+        self.numEaten = 0
+
+    def update(self, dt):
+        for powerpellet in self.powerpellets:
+            powerpellet.update(dt)
+
+    def createPelletList(self, pelletfile):
+        data = self.readPelletfile(pelletfile)
+        for row in range(data.shape[0]):
+            for col in range(data.shape[1]):
+                if data[row][col] in ['.', '+']:
+                    self.pelletList.append(Pellet(row, col))
+                elif data[row][col] in ['P', 'p']:
+                    pp = PowerPellet(row, col)
+                    self.pelletList.append(pp)
+                    self.powerpellets.append(pp)
+
+    def readPelletfile(self, textfile):
+        return np.loadtxt(textfile, dtype='<U1')
+
+    def isEmpty(self):
+        if len(self.pelletlist) == 0:
+            return True
+        return False
+
+    def render(self, screen):
+        for pellet in self.pelletList:
+            pellet.render(screen)
